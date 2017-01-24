f1ccaf46ef963b8133485622a1e02f4e2cfcf7df
mohan <mohan@cs.ucalgary.ca>
2017-01-24 14:51:13 -0700

    implemented a chromatic aberration shader

--- boilerplate/framebufferfragment.glsl
+++ boilerplate/framebufferfragment.glsl
@@ -14,5 +14,17 @@ out vec4 outColor;
 uniform sampler2D texFramebuffer;
 void main()
 {
-    outColor = texture(texFramebuffer, Texcoord);
+    float redAberration = 32.0;
+    float greenAberration = 0.0;
+    float blueAberration = -32.0;
+
+    vec3 aberration = vec3(redAberration / 1024, greenAberration / 1024, blueAberration / 1024);
+	aberration*=abs(Texcoord.x-.5)*abs(Texcoord.y-.5);
+
+    vec3 col;
+    col.r = texture2D(texFramebuffer,vec2(Texcoord.x+aberration.x,Texcoord.y)).x;
+    col.g = texture2D(texFramebuffer,vec2(Texcoord.x+aberration.y,Texcoord.y)).y;
+    col.b = texture2D(texFramebuffer,vec2(Texcoord.x+aberration.z,Texcoord.y)).z;
+
+	outColor=vec4(col, 1);
 }
