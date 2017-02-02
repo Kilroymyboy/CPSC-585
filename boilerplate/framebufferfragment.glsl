b4c82476545cbb32b261dd71bd1cbfa1bfd4ae8d
mohan <mohan@cs.ucalgary.ca>
2017-02-02 12:06:37 -0700

    got CA again

--- boilerplate/framebufferfragment.glsl
+++ boilerplate/framebufferfragment.glsl
@@ -14,17 +14,17 @@ out vec4 outColor;
 uniform sampler2D texFramebuffer;
 void main()
 {
-    float redAberration = .002;
-    float greenAberration = 0.0;
-    float blueAberration = -.002;
+    float redAberration = 1;
+    float greenAberration = 0;
+    float blueAberration = -1;
 
     vec3 aberration = vec3(redAberration, greenAberration, blueAberration);
-	aberration*=abs(Texcoord.x-.5f);
+	aberration*=abs(Texcoord.x-640)/640;
 
     vec3 col;
-//    col.x = texture(texFramebuffer,vec2(Texcoord.x+aberration.x,Texcoord.y)).x;
-//    col.y = texture(texFramebuffer,vec2(Texcoord.x+aberration.y,Texcoord.y)).y;
-//    col.z = texture(texFramebuffer,vec2(Texcoord.x+aberration.z,Texcoord.y)).z;
-	col.xyz = texelFetch(texFramebuffer,ivec2(Texcoord.x,Texcoord.y), 0).xyz;
+    col.x = texelFetch(texFramebuffer,ivec2(Texcoord.x+aberration.x,Texcoord.y),0).x;
+    col.y = texelFetch(texFramebuffer,ivec2(Texcoord.x+aberration.y,Texcoord.y),0).y;
+    col.z = texelFetch(texFramebuffer,ivec2(Texcoord.x+aberration.z,Texcoord.y),0).z;
+//	col.xyz = texelFetch(texFramebuffer,ivec2(Texcoord.x,Texcoord.y), 0).xyz;
 	outColor=vec4(col, 1);
 }
