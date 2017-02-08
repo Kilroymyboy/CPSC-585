699266adf28b2c78bfaa9fabf2f1ff7393b91139
Brad Ritten <brad.ritten@gmail.com>
2017-02-07 20:29:22 -0700

    added and linked physx

--- boilerplate/Main.cpp
+++ boilerplate/Main.cpp
@@ -1,14 +1,19 @@
 #include "Graphics.h"
 #include "Resources.h"
 #include "Game.h"
+#include "PhysicsManager.h"
 
 int main(int argc, char *argv[])
 {
 	if (Graphics::init() == -1)return -1;
+	//PhysicsManager *mPhysx = new PhysicsManager();
+
+	//mPhysx->initPhysX();
 	Light::init();
 	Viewport::init();
 	Resources::init();
 	Game::init();
+	
 	while (!Graphics::shouldClose()) {
 		Graphics::clearFrameBuffer();
 		Game::update();
