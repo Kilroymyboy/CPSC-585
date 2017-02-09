1b3cecd41c7c65e6e04c884bcf8a370a9cd6c390
mohan <mohan@cs.ucalgary.ca>
2017-02-09 16:06:17 -0700

    kinda got something.. need to work on setting up fbos properly

---
+++ boilerplate/additive.glsl
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Vertex program for barebones GLFW boilerplate
+//
+// Author:  Sonny Chan, University of Calgary
+// Date:    December 2015
+// ==========================================================================
+#version 410
+
+#extension GL_ARB_explicit_uniform_location : enable
+#extension GL_ARB_explicit_attrib_location : enable
+
+in vec2 Texcoord;
+out vec4 outColor;
+
+// large texture to downsample
+uniform sampler2D tex0;
+uniform sampler2D tex1;
+
+
+void main()
+{
+	outColor=texture(tex0, Texcoord)+texture(tex1, Texcoord);
+	outColor.w=1;
+}
