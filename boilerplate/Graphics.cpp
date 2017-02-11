786682478c90a96823aec857489918a21990271d
Brad Ritten <brad.ritten@gmail.com>
2017-02-10 18:11:43 -0700

    fixed rotations

--- boilerplate/Graphics.cpp
+++ boilerplate/Graphics.cpp
@@ -845,7 +845,7 @@ namespace Viewport {
 	void update(mat4 obj) {
 		//	transform = lookAt(vec3(cos(6 / 1.5f) * 4.0f, 1, sin(6 / 1.5f) * 4.0f), vec3(0, 0, 0), vec3(0, 1, 0));
 		transform = lookAt(vec3(cos(glfwGetTime() / 1.5f) * 4.5f, 3, sin(glfwGetTime() / 1.5f) * 4.5f), vec3(0, 0, 0), vec3(0, 1, 0));
-			transform = lookAt(vec3(0, 1.5f, -5.5f), vec3(0, 1.5f, 0), vec3(0, 1, 0));
+		//	transform = lookAt(vec3(0, 1.5f, -5.5f), vec3(0, 1.5f, 0), vec3(0, 1, 0));
 		//	transform = lookAt(vec3(10, 10.5f, -10.f), vec3(0, 0, 0), vec3(0, 1, 0));
 
 		glUniformMatrix4fv(MODEL_LOCATION, 1, false, &obj[0][0]);
