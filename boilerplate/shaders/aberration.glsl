970c09da3647631024483bd56ac8b3348871149b
mohan <mohan@cs.ucalgary.ca>
2017-03-06 11:17:30 -0700

    Revert "lens aberration"
    This reverts commit 5216d4555421f0d6215ab000198f0c66e730e315.

--- boilerplate/shaders/aberration.glsl
+++ boilerplate/shaders/aberration.glsl
@@ -8,11 +8,6 @@ out vec4 outColor;
 uniform sampler2D texFramebuffer;
 void main()
 {
-	float dist=(Texcoord.x-0.5)*(Texcoord.x-0.5)+(Texcoord.y-0.5)*(Texcoord.y-0.5);
-	float f=1+dist*(.25+.01*sqrt(dist));
-
-	vec2 coord=(Texcoord-vec2(.5,.5))*f*.9+vec2(.5,.5);
-
     float redAberration = .003;
     float greenAberration = 0.0;
     float blueAberration = -.003;
@@ -21,9 +16,11 @@ void main()
 	aberration*=abs(Texcoord.x-.5f);
 
     vec3 col;
-    col.x = texture(texFramebuffer,vec2(coord.x+aberration.x,coord.y)).x;
-    col.y = texture(texFramebuffer,vec2(coord.x+aberration.y,coord.y)).y;
-    col.z = texture(texFramebuffer,vec2(coord.x+aberration.z,coord.y)).z;
+    col.x = texture(texFramebuffer,vec2(Texcoord.x+aberration.x,Texcoord.y)).x;
+    col.y = texture(texFramebuffer,vec2(Texcoord.x+aberration.y,Texcoord.y)).y;
+    col.z = texture(texFramebuffer,vec2(Texcoord.x+aberration.z,Texcoord.y)).z;
+
+//	col.xyz = vec3(texture(texFramebuffer,vec2(Texcoord)).x);
 
 	outColor=vec4(col, 1);
 }
