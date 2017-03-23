92c27027025c12520fbe1f1e6be19a1a58e1854c
Tram <ttlenguy@ucalgary.ca>
2017-03-22 22:43:59 -0600

    readdded PowerUp.cpp/h

---
+++ boilerplate/PowerUp.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Entity.h"
+#include "Resources.h"
+#include "Graphics.h"
+#include "FilterGroup.h"
+
+class PowerUp : public Entity {
+	physx::PxRigidDynamic *actor;
+	double countDown = 8.0;
+	double deleteTime;
+	PxVec3 direction;
+	PxTransform t;
+
+	int powerId;
+	int pseudoRand();
+
+public:
+	PowerUp();
+	void update(glm::mat4 parentTransform)override;
+	void render(glm::mat4 parentTransform)override;
+	physx::PxRigidDynamic *const getActor();
+	PxVec3 getRandLocation();
+};
