9fbfb03cd68753689547d51ed7a39ef172da960e
Brad Ritten <brad.ritten2@cs.ucalgary.ca>
2017-03-23 16:55:06 -0600

    SOUND DISABLED

--- boilerplate/Main.cpp
+++ boilerplate/Main.cpp
@@ -6,7 +6,7 @@
 #include "InputManager.h"
 #include "Main.h"
 #include "HUD.h"
-#include "Sound.h"
+//#include "Sound.h"
 
 
 using namespace std;
@@ -72,8 +72,8 @@ int main(int argc, char *argv[])
 	Time::init();
 	Keyboard::init();
 	Game::init();
-	Sound::init();
-	Sound::playSound(1);
+	//Sound::init();
+	//Sound::playSound(1);
 	
 //	Sound::list_audio_devices(Sound::device);
 	while (!Graphics::shouldClose()) {
