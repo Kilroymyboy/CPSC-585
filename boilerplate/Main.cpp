f53dc8189730e363fe141b449b9c1fa2ba19e4d7
jyklau <jyklau@cs.ucalgary.ca>
2017-03-24 17:34:09 -0600

    additional changes

--- boilerplate/Main.cpp
+++ boilerplate/Main.cpp
@@ -11,52 +11,7 @@
 
 using namespace std;
 
-// create gamepad instance (controller no. 1)
-Gamepad myGamepad = Gamepad(1);
 HUD *hud;
-
-// A function to obtain input, called each frame
-void GetGamepadInput()
-{
-	//NOTE gamepad update() must be called BEFORE any pulling inputs
-
-	if (myGamepad.GetButtonPressed(Xbuttons.A)) {
-		cout << "A pressed" << endl;
-	}
-	else if (myGamepad.GetButtonPressed(Xbuttons.X)) {
-		cout << "X pressed " << endl;
-	}
-	else if (myGamepad.GetButtonPressed(Xbuttons.Y) ) {
-		cout << "Y pressed" << endl;
-	}
-	else if (myGamepad.GetButtonPressed(Xbuttons.B)) {
-		cout << "B pressed" << endl;
-	}
-	else if (myGamepad.LeftTrigger() > 0)
-	{
-		cout << "Left Trigger " << endl;
-	}
-	else if (myGamepad.RightTrigger() > 0)
-	{
-		cout << "Right Trigger " << endl;
-	}
-	else if (myGamepad.GetButtonPressed(Xbuttons.Start))
-	{
-		cout << "GAME START!!!!" << endl;
-	}
-	//four test calls to get stick x y every time implement it later for what we need to control the car but they all work
-	//myGamepad.RightStick_X();
-	//myGamepad.RightStick_Y();
-	//myGamepad.LeftStick_X();
-	//myGamepad.LeftStick_Y();
-	
-	// Update the gamepad for next frame
-	myGamepad.RefreshState();
-
-}
-
-Gamepad gamepad = Gamepad(1);
-Gamepad gamepad2 = Gamepad(2);
 
 int main(int argc, char *argv[])
 {
